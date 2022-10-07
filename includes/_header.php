9938933b49c6bfbfcc50fd0ebd673f8b9eb18dc2
Arthur15y <105573984+Arthur15y@users.noreply.github.com>
2022-10-07 09:41:05 -0300

    Dev-header

--- includes/_header.php
+++ includes/_header.php
@@ -4,14 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="assents/css/style.css">
     <title>Document</title>
 </head>
 <body>
     <header>
-        <ul>
-            <li><a href="./index.php">Index</a></li>
-            <li><a href="./noticia.php">Noticia</a></li>
-            <li><a href="./editoria.php">Editoria</a></li>
-        </ul>
+   <div class="linha-azul">
+<div class="menu-retratil">
+<input type="checkbox" id="check">
+<label for="check" id="icone"><img src="https://i.imgur.com/RWHvwPE.png"/></label>
+<div class="barra">	
+	<nav>
+		<a href=""><div class="link">Home</div></a>
+		<a href=""><div class="link">Futebol</div></a>
+		<a href=""><div class="link">Musicas</div></a>
+		<a href=""><div class="link">Jogos</div></a>
+		<a href=""><div class="link">Contato</div></a>
+	</nav>	
+</div>
+
+</div>
+
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+    </ul>
+</nav>
+<div class="search-bar">
+<input type="text" placeholder="    Pesquisar">
+<div class="lupa"><button>🔎</button></div>
+</div>
+   </div>
     </header>
