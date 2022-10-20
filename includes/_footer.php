601cf193a8688972395f02345c3c9a09d27102c4
Yasser <103062506+Kleidin@users.noreply.github.com>
2022-10-20 09:12:30 -0300

    Dev-footer
    pronto

--- includes/_footer.php
+++ includes/_footer.php
@@ -1,6 +1,5 @@
  
     <footer>
-        <div class="linha"><hr></div>
         <div class="artigos">
         <h1>Artigos</h1>
         <hr>
@@ -26,6 +25,11 @@
             <h4>Email</h4>
             <h4>Telefone</h4>
             <h4>Whatsapp</h4>
+        </div>
+        <div class="icones">
+            <img src="https://logopng.com.br/logos/facebook-13.png">
+            <img src="https://cdn-icons-png.flaticon.com/512/124/124021.png">
+            <img src="https://imagepng.org/wp-content/uploads/2017/08/instagram-icone-icon-1.png">
         </div>
     </footer>
 
--- includes/_header.php
+++ includes/_header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./assents/css/style.css?v=<?php echo rand(0,9999999);?>">
     <title>Document</title>
 </head>
 <body>
