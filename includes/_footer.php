cfd74909727267479093fc1aeccf3faba6e7d160
Arthur15y <Arthurmellopimentel76@gmail.com>
2022-10-21 08:48:45 -0300

    footer

--- includes/_footer.php
+++ includes/_footer.php
@@ -1,6 +1,31 @@
- 
-    <footer>
-        <h3>Yasser fonseca</h3>
+<footer>
+        <div class="artigos">
+        <h1>Artigos</h1>
+        <h4>Skins</h4>
+        <h4>Partidas</h4>
+        <h4>Mapas</h4>
+        </div>
+        <div class="ajuda">
+            <h1>Ajuda</h1>
+            <h4>Contate-nos</h4>
+        </div>
+        <div class="jn">
+            <h1>JNCultura</h1>
+            <h4>Sobre Nós</h4>
+            <h4>Patrocinio</h4>
+            <h4>Blogs</h4>
+        </div>
+        <div class="contato">
+            <h1>Contato</h1>
+            <h4>Email</h4>
+            <h4>Telefone</h4>
+            <h4>Whatsapp</h4>
+        </div>
+        <div class="icones">
+            <img src="https://logopng.com.br/logos/facebook-13.png">
+            <img src="https://cdn-icons-png.flaticon.com/512/124/124021.png">
+            <img src="https://imagepng.org/wp-content/uploads/2017/08/instagram-icone-icon-1.png">
+        </div>
     </footer>
 
 <script src="./index.js"></script>
