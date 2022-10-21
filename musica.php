6a483a75ba2146fa6529028d59130ca789e79f7f
Arthur15y <Arthurmellopimentel76@gmail.com>
2022-10-21 10:51:15 -0300

    Atualizações

---
+++ musica.php
@@ -0,0 +1,60 @@
+<?php
+include_once './includes/_header.php';
+?>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+<main class="main-musica">
+<div class="img1">
+    img
+</div>
+<p class="txt1">
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus consequatur sed itaque numquam doloribus rem at repudiandae? Illum debitis numquam assumenda nam, ducimus fugiat harum aliquid, nemo, aliquam porro nisi.
+</p>
+<p class="txt2">
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus consequatur sed itaque numquam doloribus rem at repudiandae? Illum debitis numquam assumenda nam, ducimus fugiat harum aliquid, nemo, aliquam porro nisi.
+</p>
+<div class="img2">
+    img
+</div>
+<div class="img3">
+    img
+</div>
+<p class="txt3">
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus consequatur sed itaque numquam doloribus rem at repudiandae? Illum debitis numquam assumenda nam, ducimus fugiat harum aliquid, nemo, aliquam porro nisi.
+</p>
+<p class="txt4">
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus consequatur sed itaque numquam doloribus rem at repudiandae? Illum debitis numquam assumenda nam, ducimus fugiat harum aliquid, nemo, aliquam porro nisi.
+</p>
+<div class="img4">
+    img
+</div>
+<div class="img5">
+    img
+</div>
+<p class="txt5">
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus consequatur sed itaque numquam doloribus rem at repudiandae? Illum debitis numquam assumenda nam, ducimus fugiat harum aliquid, nemo, aliquam porro nisi.
+</p>
+
+
+
+</main>
+
+<aside class="aside-musica">
+ 
+</aside>
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    
+<?php
+
+
+
+include_once './includes/_footer.php';
+?>
